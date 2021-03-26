13513624f67fe14591eba2bf9498147417c248cc
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-03-26 13:19:38 +0000

    Add features to upload and list files

--- yagdrive/core.py
+++ yagdrive/core.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+ROOT_FOLDER_ID = 'root'
+
 
 class GDrive:
     def __init__(
@@ -14,16 +16,24 @@ class GDrive:
         gauth = GoogleAuth()
         gauth.LoadCredentialsFile(credentials_file)
         self.drive = GoogleDrive(gauth)
+        self.cwd = 'root'
 
-    def upload(self, source_path: str, title=None, folder_id='root'):
+    def put(self, source_path: str, title=None, overwrite=True):
         source_file = Path(source_path)
         title = title or source_file.name
         file = self.drive.CreateFile(
             {
                 'title': title,
-                'parents': [{'id': folder_id}],
+                'parents': [{'id': self.cwd}],
             }
         )
         file.SetContentFile(source_file)
         file.Upload()
         return file['id']
+
+    def ls(self):
+        q = f"'{self.cwd}' in parents and trashed=False"
+        return self.drive.ListFile({'q': q}).GetList()
+
+    def cd(self, folder_id='root'):
+        self.cwd = folder_id
